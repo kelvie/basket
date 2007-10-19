b3afbbd7d9cdf4456c92182b37acd575f9dc900b
vtokarev <vtokarev@68e0c82f-6715-0410-a4b6-c4571a790354>
2007-10-19 19:02:55 +0000

    git-svn-id: http://basket.linux62.org/svn/basket/branches/kde4port@573 68e0c82f-6715-0410-a4b6-c4571a790354

--- src/focusedwidgets.h
+++ src/focusedwidgets.h
@@ -35,10 +35,10 @@ class FocusedTextEdit : public KTextEdit
   public:
 	FocusedTextEdit(bool disableUpdatesOnKeyPress, QWidget *parent = 0, const char *name = 0);
 	~FocusedTextEdit();
-	void paste();
+	//TODO void paste();
 	QTextCursor* textCursor() const;
   protected:
-	void adaptClipboardText(QClipboard::Mode mode);
+	//TODO void adaptClipboardText(QClipboard::Mode mode);
 	void keyPressEvent(QKeyEvent *event);
 	void wheelEvent(QWheelEvent *event);
 	void enterEvent(QEvent *event);
