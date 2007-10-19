666f490eaf2bbfd838064a2850ae2d12f0793adf
vtokarev <vtokarev@68e0c82f-6715-0410-a4b6-c4571a790354>
2007-10-19 19:07:54 +0000

    git-svn-id: http://basket.linux62.org/svn/basket/branches/kde4port@580 68e0c82f-6715-0410-a4b6-c4571a790354

--- src/exporterdialog.cpp
+++ src/exporterdialog.cpp
@@ -33,40 +33,41 @@
 #include <kvbox.h>
 
 ExporterDialog::ExporterDialog(Basket *basket, QWidget *parent, const char *name)
- : KDialog(parent, name, /*modal=*/true, i18n("Export Basket to HTML"),
-               KDialog::Ok | KDialog::Cancel, KDialog::Ok, /*separator=*/true),
+ :// KDialog(parent, name, /*modal=*/true, i18n("Export Basket to HTML"),
+  //             KDialog::Ok | KDialog::Cancel, KDialog::Ok, /*separator=*/true),
    m_basket(basket)
 {
-	KVBox *page  = makeVBoxMainWidget();
-
-	QWidget     *wid  = new QWidget(page);
-	QHBoxLayout *hLay = new QHBoxLayout(wid);
-	hLay->setMargin(0);
-	hLay->setSpacing(KDialog::spacingHint());
-	m_url = new KUrlRequester("", wid);
-	m_url->setCaption(i18n("HTML Page Filename"));
-	m_url->setFilter("text/html");
-	m_url->fileDialog()->setOperationMode(KFileDialog::Saving);
-	hLay->addWidget( new QLabel(m_url, i18n("&Filename:"), wid) );
-	hLay->addWidget( m_url );
-
-	m_embedLinkedFiles    = new QCheckBox(i18n("&Embed linked local files"),              page);
-	m_embedLinkedFolders  = new QCheckBox(i18n("Embed &linked local folders"),            page);
-	m_erasePreviousFiles  = new QCheckBox(i18n("Erase &previous files in target folder"), page);
-	m_formatForImpression = new QCheckBox(i18n("For&mat for impression"),                 page);
-	m_formatForImpression->hide();
-
-	load();
-	m_url->lineEdit()->setFocus();
-
-	showTile(true);
-	// Add a stretch at the bottom:
-	// Duplicated code from AddBasketWizard::addStretch(QWidget *parent):
-	(new QWidget(page))->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-	// Double the width, because the filename should be visible
-	QSize size(sizeHint());
-	resize(QSize(size.width() * 2, size.height()));
+	//TODO
+//	KVBox *page  = makeVBoxMainWidget();
+//
+//	QWidget     *wid  = new QWidget(page);
+//	QHBoxLayout *hLay = new QHBoxLayout(wid);
+//	hLay->setMargin(0);
+//	hLay->setSpacing(KDialog::spacingHint());
+//	m_url = new KUrlRequester("", wid);
+//	m_url->setCaption(i18n("HTML Page Filename"));
+//	m_url->setFilter("text/html");
+//	m_url->fileDialog()->setOperationMode(KFileDialog::Saving);
+//	hLay->addWidget( new QLabel(m_url, i18n("&Filename:"), wid) );
+//	hLay->addWidget( m_url );
+//
+//	m_embedLinkedFiles    = new QCheckBox(i18n("&Embed linked local files"),              page);
+//	m_embedLinkedFolders  = new QCheckBox(i18n("Embed &linked local folders"),            page);
+//	m_erasePreviousFiles  = new QCheckBox(i18n("Erase &previous files in target folder"), page);
+//	m_formatForImpression = new QCheckBox(i18n("For&mat for impression"),                 page);
+//	m_formatForImpression->hide();
+//
+//	load();
+//	m_url->lineEdit()->setFocus();
+//
+//	showTile(true);
+//	// Add a stretch at the bottom:
+//	// Duplicated code from AddBasketWizard::addStretch(QWidget *parent):
+//	(new QWidget(page))->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+//
+//	// Double the width, because the filename should be visible
+//	QSize size(sizeHint());
+//	resize(QSize(size.width() * 2, size.height()));
 /*
 ==========================
 + [00000000000          ] Progress bar!
@@ -80,50 +81,50 @@ ExporterDialog::~ExporterDialog()
 
 void ExporterDialog::show()
 {
-	KDialog::show();
-
-	QString lineEditText = m_url->lineEdit()->text();
-	int selectionStart = lineEditText.findRev("/") + 1;
-	m_url->lineEdit()->setSelection(selectionStart, lineEditText.length() - selectionStart - QString(".html").length());
+//	KDialog::show();
+//
+//	QString lineEditText = m_url->lineEdit()->text();
+//	int selectionStart = lineEditText.findRev("/") + 1;
+//	m_url->lineEdit()->setSelection(selectionStart, lineEditText.length() - selectionStart - QString(".html").length());
 }
 
 void ExporterDialog::load()
 {
-	KConfig *config = KGlobal::config();
-	config->setGroup("HTML Export");
-
-	QString folder = config->readEntry("lastFolder", QDir::homePath()) + "/";
-	QString url = folder + QString(m_basket->basketName()).replace("/", "_") + ".html";
-	m_url->setURL(url);
-
-	m_embedLinkedFiles->setChecked(    config->readBoolEntry("embedLinkedFiles",    true)                      );
-	m_embedLinkedFolders->setChecked(  config->readBoolEntry("embedLinkedFolders",  false)                     );
-	m_erasePreviousFiles->setChecked(  config->readBoolEntry("erasePreviousFiles",  true)                      );
-	m_formatForImpression->setChecked( config->readBoolEntry("formatForImpression", false)                     );
+//	KConfig *config = KGlobal::config();
+//	config->setGroup("HTML Export");
+//
+//	QString folder = config->readEntry("lastFolder", QDir::homePath()) + "/";
+//	QString url = folder + QString(m_basket->basketName()).replace("/", "_") + ".html";
+//	m_url->setURL(url);
+//
+//	m_embedLinkedFiles->setChecked(    config->readBoolEntry("embedLinkedFiles",    true)                      );
+//	m_embedLinkedFolders->setChecked(  config->readBoolEntry("embedLinkedFolders",  false)                     );
+//	m_erasePreviousFiles->setChecked(  config->readBoolEntry("erasePreviousFiles",  true)                      );
+//	m_formatForImpression->setChecked( config->readBoolEntry("formatForImpression", false)                     );
 }
 
 void ExporterDialog::save()
 {
-	KConfig *config = KGlobal::config();
-	config->setGroup("HTML Export");
-
-	QString folder = KUrl(m_url->url()).directory();
-	config->writeEntry( "lastFolder",          folder                             );
-	config->writeEntry( "embedLinkedFiles",    m_embedLinkedFiles->isChecked()    );
-	config->writeEntry( "embedLinkedFolders",  m_embedLinkedFolders->isChecked()  );
-	config->writeEntry( "erasePreviousFiles",  m_erasePreviousFiles->isChecked()  );
-	config->writeEntry( "formatForImpression", m_formatForImpression->isChecked() );
+//	KConfig *config = KGlobal::config();
+//	config->setGroup("HTML Export");
+//
+//	QString folder = KUrl(m_url->url()).directory();
+//	config->writeEntry( "lastFolder",          folder                             );
+//	config->writeEntry( "embedLinkedFiles",    m_embedLinkedFiles->isChecked()    );
+//	config->writeEntry( "embedLinkedFolders",  m_embedLinkedFolders->isChecked()  );
+//	config->writeEntry( "erasePreviousFiles",  m_erasePreviousFiles->isChecked()  );
+//	config->writeEntry( "formatForImpression", m_formatForImpression->isChecked() );
 }
 
 void ExporterDialog::slotOk()
 {
-	save();
-	KDialog::slotOk();
+//	save();
+//	KDialog::slotOk();
 }
 
 QString ExporterDialog::filePath()
 {
-	return m_url->url();
+//	return m_url->url();
 }
 
 bool ExporterDialog::embedLinkedFiles()
