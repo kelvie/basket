91f6cd93fb1debe9d0ba0213ed9fd8a40acf5dfc
vtokarev <vtokarev@68e0c82f-6715-0410-a4b6-c4571a790354>
2007-11-27 01:53:27 +0000

    removed plasma stuff
    git-svn-id: http://basket.linux62.org/svn/basket/branches/basket2@611 68e0c82f-6715-0410-a4b6-c4571a790354

--- src/basketviewcontainer.cpp
+++ src/basketviewcontainer.cpp
@@ -6,20 +6,12 @@
 #include <KDebug>
 
 #include "basketviewcontainer.h"
-#include "plasma/corona.h"
-#include "plasma/containment.h"
 
 BasketViewContainer::BasketViewContainer() : QStackedWidget() {
 	QGraphicsView *view = new QGraphicsView();
 	this->addWidget( view );
 	
-	Plasma::Corona *corona = new Plasma::Corona();
-	//Plasma::Containment *cont = corona->addContainment( "context" );
-	Plasma::Containment *cont = corona->addContainment( "content" );
-	cont->setScreen( 0 );
-	cont->setFormFactor( Plasma::Planar );
-
-	view->setScene( corona );
+	view->setScene( new QGraphicsScene );
 }
 
 BasketViewContainer::~BasketViewContainer() {
