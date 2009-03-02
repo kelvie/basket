kelvie/basket

commit 516b7c78ec521296b91a7a99c04845288a59c293

Replace deprecated Q3ValueList with QList.
Also converted all the Q3ValueStacks to QList.
This might even help performance some!

src/note.cpp
@@ -21,7 +21,7 @@
 #include <qpainter.h>
 //Added by qt3to4:
 #include <QPixmap>
-#include <Q3ValueList>
+#include <QList>
 #include <qstyle.h>
 #include <kapplication.h>
 #include <kstyle.h>
@@ -287,7 +287,7 @@ void Note::selectIn(const QRect &rect, bool invertSelection, bool unselectOthers
 	// Only intersects with visible areas.
 	// If the note is not visible, the user don't think it will be selected while selecting the note(s) that hide this, so act like the user think:
 	bool intersects = false;
-	for (Q3ValueList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
+	for (QList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
 		QRect &r = *it;
 		if (r.intersects(rect)) {
 			intersects = true;
@@ -1035,7 +1035,7 @@ Note* Note::noteAt(int x, int y)
 		if ((x >= right) && (x < right + RESIZER_WIDTH) && (y >= m_y) && (y < m_y + resizerHeight())) {
 			if ( ! m_computedAreas )
 				recomputeAreas();
-			for (Q3ValueList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
+			for (QList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
 				QRect &rect = *it;
 				if (rect.contains(x, y))
 					return this;
@@ -1047,7 +1047,7 @@ Note* Note::noteAt(int x, int y)
 		if ((x >= m_x) && (x < m_x + width()) && (y >= m_y) && (y < m_y + m_height)) {
 			if ( ! m_computedAreas )
 				recomputeAreas();
-			for (Q3ValueList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
+			for (QList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
 				QRect &rect = *it;
 				if (rect.contains(x, y))
 					return this;
@@ -1069,7 +1069,7 @@ Note* Note::noteAt(int x, int y)
 	} else if (matching() && y >= m_y && y < m_y + m_height && x >= m_x && x < m_x + m_width) {
 		if ( ! m_computedAreas )
 			recomputeAreas();
-		for (Q3ValueList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
+		for (QList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
 			QRect &rect = *it;
 			if (rect.contains(x, y))
 				return this;
@@ -1606,39 +1606,40 @@ void Note::setOnTop(bool onTop)
 	}
 }
 
-void substractRectOnAreas(const QRect &rectToSubstract, Q3ValueList<QRect> &areas, bool andRemove)
+void substractRectOnAreas(const QRect &rectToSubstract, QList<QRect> &areas, bool andRemove)
 {
-	for (Q3ValueList<QRect>::iterator it = areas.begin(); it != areas.end(); ) {
-		QRect &rect = *it;
+	for (QList<QRect>::iterator it = areas.begin(); it != areas.end();){
+		QRect& rect = *it;
 		// Split the rectangle if it intersects with rectToSubstract:
 		if (rect.intersects(rectToSubstract)) {
 			// Create the top rectangle:
 			if (rectToSubstract.top() > rect.top()) {
-				areas.insert(it, QRect(rect.left(), rect.top(), rect.width(), rectToSubstract.top() - rect.top()));
+				it = areas.insert(it,QRect(rect.left(), rect.top(), rect.width(), rectToSubstract.top() - rect.top()));
 				rect.setTop(rectToSubstract.top());
 			}
 			// Create the bottom rectangle:
 			if (rectToSubstract.bottom() < rect.bottom()) {
-				areas.insert(it, QRect(rect.left(), rectToSubstract.bottom() + 1, rect.width(), rect.bottom() - rectToSubstract.bottom()));
+				it = areas.insert(it, QRect(rect.left(), rectToSubstract.bottom() + 1, rect.width(), rect.bottom() - rectToSubstract.bottom()));
 				rect.setBottom(rectToSubstract.bottom());
 			}
 			// Create the left rectangle:
 			if (rectToSubstract.left() > rect.left()) {
-				areas.insert(it, QRect(rect.left(), rect.top(), rectToSubstract.left() - rect.left(), rect.height()));
+				it = areas.insert(it, QRect(rect.left(), rect.top(), rectToSubstract.left() - rect.left(), rect.height()));
 				rect.setLeft(rectToSubstract.left());
 			}
 			// Create the right rectangle:
 			if (rectToSubstract.right() < rect.right()) {
-				areas.insert(it, QRect(rectToSubstract.right() + 1, rect.top(), rect.right() - rectToSubstract.right(), rect.height()));
+				it = areas.insert(it, QRect(rectToSubstract.right() + 1, rect.top(), rect.right() - rectToSubstract.right(), rect.height()));
 				rect.setRight(rectToSubstract.right());
 			}
 			// Remove the rectangle if it's entirely contained:
 			if (andRemove && rectToSubstract.contains(rect))
-				it = areas.remove(it);
-			else
+				it = areas.erase(it);
+			else 
 				++it;
-		} else
-			++it;
+		}
+		else
+			++it;	
 	}
 }
 
@@ -1796,7 +1797,7 @@ void Note::draw(QPainter *painter, const QRect &clipRect)
 				recomputeAreas();
 			if (m_areas.isEmpty())
 				return;
-			for (Q3ValueList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
+			for (QList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
 				QRect &rect = *it;
 				painter->drawPixmap(rect.x(), rect.y(), pixmap, rect.x() - right, rect.y() - y(), rect.width(), rect.height());
 			}
@@ -2002,7 +2003,7 @@ void Note::draw(QPainter *painter, const QRect &clipRect)
 
 void Note::drawBufferOnScreen(QPainter *painter, const QPixmap &contentPixmap)
 {
-	for (Q3ValueList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
+	for (QList<QRect>::iterator it = m_areas.begin(); it != m_areas.end(); ++it) {
 		QRect &rect = *it;
 		if (rect.x() >= x() + width()) // It's a rect of the resizer, don't draw it!
 			continue;
@@ -2143,12 +2144,12 @@ void Note::recomputeAllStyles()
 			child->recomputeAllStyles();
 }
 
-bool Note::removedStates(const Q3ValueList<State*> &deletedStates)
+bool Note::removedStates(const QList<State*> &deletedStates)
 {
 	bool modifiedBasket = false;
 
 	if (!states().isEmpty()) {
-		for (Q3ValueList<State*>::const_iterator it = deletedStates.begin(); it != deletedStates.end(); ++it)
+		for (QList<State*>::const_iterator it = deletedStates.begin(); it != deletedStates.end(); ++it)
 			if (hasState(*it)) {
 				removeState(*it);
 				modifiedBasket = true;
@@ -2367,7 +2368,7 @@ void Note::bufferizeSelectionPixmap()
 
 QRect Note::visibleRect()
 {
-	Q3ValueList<QRect> areas;
+	QList<QRect> areas;
 	areas.append(rect());
 
 	// When we are folding a parent group, if this note is bigger than the first real note of the group, cut the top of this:
@@ -2379,12 +2380,12 @@ QRect Note::visibleRect()
 	}
 
 	if (areas.count() > 0)
-		return areas[0];
+		return areas.first();
 	else
 		return QRect();
 }
 
-void Note::recomputeBlankRects(Q3ValueList<QRect> &blankAreas)
+void Note::recomputeBlankRects(QList<QRect> &blankAreas)
 {
 	if (!matching())
 		return;
@@ -2435,7 +2436,7 @@ Note* Note::noteForFullPath(const QString &path)
 	return 0;
 }
 
-void Note::listUsedTags(Q3ValueList<Tag*> &list)
+void Note::listUsedTags(QList<Tag*> &list)
 {
 	for (State::List::Iterator it = m_states.begin(); it != m_states.end(); ++it) {
 		Tag *tag = (*it)->parentTag();
@@ -2448,7 +2449,7 @@ void Note::listUsedTags(Q3ValueList<Tag*> &list)
 }
 
 
-void Note::usedStates(Q3ValueList<State*> &states)
+void Note::usedStates(QList<State*> &states)
 {
 	if (content())
 		for (State::List::Iterator it = m_states.begin(); it != m_states.end(); ++it)

src/archive.cpp
@@ -19,8 +19,8 @@
  ***************************************************************************/
 
 #include <qstring.h>
-#include <qstringlist.h>
-#include <q3valuelist.h>
+#include <QStringList>
+#include <QList>
 #include <qmap.h>
 #include <qdir.h>
 //Added by qt3to4:
@@ -92,7 +92,7 @@ void Archive::save(Basket *basket, bool withSubBaskets, const QString &destinati
 	dir.remove(tempFolder + "baskets.xml");
 
 	// Save a Small tags.xml Document:
-	Q3ValueList<Tag*> tags;
+	QList<Tag*> tags;
 	listUsedTags(basket, withSubBaskets, tags);
 	Tag::saveTagsTo(tags, tempFolder + "tags.xml");
 	tar.addLocalFile(tempFolder + "tags.xml", "tags.xml");
@@ -244,7 +244,7 @@ void Archive::saveBasketToArchive(Basket *basket, bool recursive, KTar *tar, QSt
 	}
 }
 
-void Archive::listUsedTags(Basket *basket, bool recursive, Q3ValueList<Tag*> &list)
+void Archive::listUsedTags(Basket *basket, bool recursive, QList<Tag*> &list)
 {
 	basket->listUsedTags(list);
 	BasketListViewItem *item = Global::bnpView->listViewItemForBasket(basket);

src/archive.h
@@ -21,7 +21,7 @@
 #ifndef ARCHIVE_H
 #define ARCHIVE_H
 
-#include <q3valuelist.h>
+#include <QList>
 #include <qmap.h>
 
 class Basket;
@@ -44,7 +44,7 @@ class Archive
   private:
 	// Convenient Methods for Saving:
 	static void saveBasketToArchive(Basket *basket, bool recursive, KTar *tar, QStringList &backgrounds, const QString &tempFolder, QProgressBar *progress);
-	static void listUsedTags(Basket *basket, bool recursive, Q3ValueList<Tag*> &list);
+	static void listUsedTags(Basket *basket, bool recursive, QList<Tag*> &list);
 	// Convenient Methods for Loading:
 	static void renameBasketFolders(const QString &extractionFolder, QMap<QString, QString> &mergedStates);
 	static void renameBasketFolder(const QString &extractionFolder, QDomNode &basketNode, QMap<QString, QString> &folderMap, QMap<QString, QString> &mergedStates);

src/basket.cpp
@@ -37,7 +37,7 @@
 #include <QDropEvent>
 #include <QDragEnterEvent>
 #include <QHBoxLayout>
-#include <Q3ValueList>
+#include <QList>
 #include <QMouseEvent>
 #include <QCloseEvent>
 #include <QGridLayout>
@@ -183,9 +183,9 @@ int NoteSelection::count()
 	return count;
 }
 
-Q3ValueList<Note*> NoteSelection::parentGroups()
+QList<Note*> NoteSelection::parentGroups()
 {
-	Q3ValueList<Note*> groups;
+	QList<Note*> groups;
 
 	// For each note:
 	for (NoteSelection *node = firstStacked(); node; node = node->nextStacked())
@@ -326,7 +326,7 @@ void drawGradient( QPainter *p, const QColor &colorTop, const QColor & colorBott
 /*
  * Defined in note.cpp:
  */
-extern void substractRectOnAreas(const QRect &rectToSubstract, Q3ValueList<QRect> &areas, bool andRemove = true);
+extern void substractRectOnAreas(const QRect &rectToSubstract, QList<QRect> &areas, bool andRemove = true);
 
 void debugZone(int zone)
 {
@@ -1786,7 +1786,7 @@ void Basket::recomputeAllStyles()
 		note->recomputeAllStyles();
 }
 
-void Basket::removedStates(const Q3ValueList<State*> &deletedStates)
+void Basket::removedStates(const QList<State*> &deletedStates)
 {
 	bool modifiedBasket = false;
 
@@ -2845,7 +2845,7 @@ void Basket::tooltipEvent(QHelpEvent *event)
 	if (!note && isFreeLayout()) {
 		message = i18n("Insert note here\nRight click for more options");
 		QRect itRect;
-		for (Q3ValueList<QRect>::iterator it = m_blankAreas.begin(); it != m_blankAreas.end(); ++it) {
+		for (QList<QRect>::iterator it = m_blankAreas.begin(); it != m_blankAreas.end(); ++it) {
 			itRect = QRect(0, 0, visibleWidth(), visibleHeight()).intersect(*it);
 			if (itRect.contains(contentPos)) {
 				rect = itRect;
@@ -3197,7 +3197,7 @@ void Basket::drawContents(QPainter *painter, int clipX, int clipY, int clipWidth
 	QPixmap  pixmap;
 	QPainter painter2;
 	QRect    rect;
-	for (Q3ValueList<QRect>::iterator it = m_blankAreas.begin(); it != m_blankAreas.end(); ++it) {
+	for (QList<QRect>::iterator it = m_blankAreas.begin(); it != m_blankAreas.end(); ++it) {
 		rect = clipRect.intersect(*it);
 		if (rect.width() > 0 && rect.height() > 0) {
 			// If there is an inserter to draw, draw the image off screen,
@@ -3349,7 +3349,7 @@ void Basket::updateNote(Note *note)
 
 void Basket::animateObjects()
 {
-	Q3ValueList<Note*>::iterator it;
+	QList<Note*>::iterator it;
 	for (it = m_animatedNotes.begin(); it != m_animatedNotes.end(); )
 //		if ((*it)->y() >= contentsY() && (*it)->bottom() <= contentsY() + contentsWidth())
 //			updateNote(*it);
@@ -3431,7 +3431,7 @@ void Basket::popupEmblemMenu(Note *note, int emblemNumber)
 		menu.addAction(act);
 	} else {
 		menu.addTitle(tag->name());
-		Q3ValueList<State*>::iterator it;
+		QList<State*>::iterator it;
 		State *currentState;
 
 		int i = 10;
@@ -4696,9 +4696,9 @@ void Basket::deleteFiles()
 	Tools::deleteRecursively(fullPath());
 }
 
-Q3ValueList<State*> Basket::usedStates()
+QList<State*> Basket::usedStates()
 {
-	Q3ValueList<State*> states;
+	QList<State*> states;
 	FOR_EACH_NOTE (note)
 		note->usedStates(states);
 	return states;
@@ -4731,7 +4731,7 @@ QString Basket::saveGradientBackground(const QColor &color, const QFont &font, c
 	return fileName;
 }
 
-void Basket::listUsedTags(Q3ValueList<Tag*> &list)
+void Basket::listUsedTags(QList<Tag*> &list)
 {
 	if (!isLoaded()) {
 		load();
@@ -5209,7 +5209,7 @@ void Basket::watchedFileDeleted(const QString &fullPath)
 
 void Basket::updateModifiedNotes()
 {
-	for (Q3ValueList<QString>::iterator it = m_modifiedFiles.begin(); it != m_modifiedFiles.end(); ++it) {
+	for (QList<QString>::iterator it = m_modifiedFiles.begin(); it != m_modifiedFiles.end(); ++it) {
 		Note *note = noteForFullPath(*it);
 		if (note)
 			note->content()->loadFromFile(/*lazyLoad=*/false);

src/basket.h
@@ -23,7 +23,7 @@
 
 #include <q3scrollview.h>
 #include <qtooltip.h>
-#include <q3valuelist.h>
+#include <QList>
 #include <qtimer.h>
 #include <qimage.h>
 #include <qdatetime.h>
@@ -118,7 +118,7 @@ class NoteSelection
 	void append(NoteSelection *node);
 	int count();
 
-	Q3ValueList<Note*> parentGroups();
+	QList<Note*> parentGroups();
 };
 
 /** This store all needed information when exporting to HTML
@@ -297,7 +297,7 @@ class Basket : public Q3ScrollView
 
 /// ANIMATIONS:
   private:
-	Q3ValueList<Note*> m_animatedNotes;
+	QList<Note*> m_animatedNotes;
 	QTimer            m_animationTimer;
 	int               m_deltaY;
 	QTime             m_lastFrameTime;
@@ -432,7 +432,7 @@ public slots:
   public slots:
 	void activatedTagShortcut(Tag *tag);
 	void recomputeAllStyles();
-	void removedStates(const Q3ValueList<State*> &deletedStates);
+	void removedStates(const QList<State*> &deletedStates);
   private slots:
 	void toggledTagInMenu(QAction *act);
 	void toggledStateInMenu(QAction *act);
@@ -473,7 +473,7 @@ public slots:
 
 /// BLANK SPACES DRAWING:
   private:
-	Q3ValueList<QRect> m_blankAreas;
+	QList<QRect> m_blankAreas;
 	void recomputeBlankRects();
 	QWidget *m_cornerWidget;
 
@@ -592,13 +592,13 @@ public slots:
 /// DRAG AND DROP:
   private:
 	bool m_isDuringDrag;
-	Q3ValueList<Note*> m_draggedNotes;
+	QList<Note*> m_draggedNotes;
   public:
 	static void acceptDropEvent(QDropEvent *event, bool preCond = true);
 	void contentsDropEvent(QDropEvent *event);
 	void blindDrop(QDropEvent* event);
 	bool isDuringDrag() { return m_isDuringDrag; }
-	Q3ValueList<Note*> draggedNotes() { return m_draggedNotes; }
+	QList<Note*> draggedNotes() { return m_draggedNotes; }
   protected:
 	void contentsDragEnterEvent(QDragEnterEvent*);
 	void contentsDragMoveEvent(QDragMoveEvent *event);
@@ -610,11 +610,11 @@ public slots:
 
 /// EXPORTATION:
   public:
-	Q3ValueList<State*> usedStates();
+	QList<State*> usedStates();
 	static QString saveGradientBackground(const QColor &color, const QFont &font, const QString &folder);
 
   public:
-	void listUsedTags(Q3ValueList<Tag*> &list);
+	void listUsedTags(QList<Tag*> &list);
 
 /// MANAGE FOCUS:
   private:
@@ -663,7 +663,7 @@ public slots:
   private:
 	KDirWatch           *m_watcher;
 	QTimer               m_watcherTimer;
-	Q3ValueList<QString>  m_modifiedFiles;
+	QList<QString>  m_modifiedFiles;
   public:
 	void addWatchedFile(const QString &fullPath);
 	void removeWatchedFile(const QString &fullPath);

src/bnpview.cpp
@@ -30,7 +30,7 @@
 //Added by qt3to4:
 #include <QResizeEvent>
 #include <QShowEvent>
-#include <Q3ValueList>
+#include <QList>
 #include <QKeyEvent>
 #include <QEvent>
 #include <QHideEvent>
@@ -287,7 +287,7 @@ void BNPView::onFirstShow()
     int treeWidth = Settings::basketTreeWidth();
     if (treeWidth < 0)
       treeWidth = m_tree->fontMetrics().maxWidth() * 11;
-    Q3ValueList<int> splitterSizes;
+    QList<int> splitterSizes;
     splitterSizes.append(treeWidth);
     setSizes(splitterSizes);
 }
@@ -1499,7 +1499,7 @@ void BNPView::recomputeAllStyles()
 	}
 }
 
-void BNPView::removedStates(const Q3ValueList<State*> &deletedStates)
+void BNPView::removedStates(const QList<State*> &deletedStates)
 {
 	QTreeWidgetItemIterator it(m_tree);
 	while (*it) {
@@ -1691,7 +1691,7 @@ void BNPView::updateNotesActions()
 	m_actMoveNoteDown    ->setEnabled( !isLocked && oneOrSeveralSelected );
 	m_actMoveOnBottom    ->setEnabled( !isLocked && oneOrSeveralSelected && !currentBasket()->isFreeLayout() );
 
-	for (QLinkedList<KAction *>::const_iterator action = m_insertActions.constBegin(); action != m_insertActions.constEnd(); ++action)
+	for (QList<KAction *>::const_iterator action = m_insertActions.constBegin(); action != m_insertActions.constEnd(); ++action)
 		(*action)->setEnabled( !isLocked );
 
 	// From the old Note::contextMenuEvent(...) :
@@ -2628,7 +2628,7 @@ void BNPView::populateTagsMenu(KMenu &menu, Note *referenceNote)
 	currentBasket()->m_tagPopupNote = referenceNote;
 	bool enable = currentBasket()->countSelecteds() > 0;
 
-	Q3ValueList<Tag*>::iterator it;
+	QList<Tag*>::iterator it;
 	Tag *currentTag;
 	State *currentState;
 	int i = 10;

src/bnpview.h
@@ -26,10 +26,9 @@
 #include <qtimer.h>
 #include <qclipboard.h>
 #include <qsplitter.h>
-#include <q3listview.h>
+#include <QTreeWidget>
 //Added by qt3to4:
-#include <Q3ValueList>
-#include <QLinkedList>
+#include <QList>
 #include <QPixmap>
 #include <QHideEvent>
 #include <QEvent>
@@ -75,7 +74,7 @@ class BASKET_EXPORT BNPView : public QSplitter
 		void setTreePlacement(bool onLeft);
 		void relayoutAllBaskets();
 		void recomputeAllStyles();
-		void removedStates(const Q3ValueList<State*> &deletedStates);
+		void removedStates(const QList<State*> &deletedStates);
 		void linkLookChanged();
 		void filterPlacementChanged(bool onTop);
 	/// MANAGE BASKETS:
@@ -268,7 +267,7 @@ class BASKET_EXPORT BNPView : public QSplitter
 		KAction       *m_actInsertLauncher;
 		KAction       *m_actImportIcon;
 		KAction       *m_actLoadFile;
-		QLinkedList<KAction*> m_insertActions;
+		QList<KAction*> m_insertActions;
 		// Basket actions :
 		KToggleAction *m_actShowFilter;
 		KAction       *m_actResetFilter;

src/htmlexporter.cpp
@@ -38,7 +38,7 @@
 #include <qpainter.h>
 //Added by qt3to4:
 #include <QTextStream>
-#include <Q3ValueList>
+#include <QList>
 #include <QPixmap>
 #include <kprogressdialog.h>
 #include <QProgressBar>
@@ -264,7 +264,7 @@ void HTMLExporter::exportBasket(Basket *basket, bool isSubBasket)
 		<< LinkLook::launcherLook->toCSS("launcher", basket->textColor())
 		<<
 		"   .unknown { margin: 1px 2px; border: 1px solid " << borderColor << "; -moz-border-radius: 4px; }\n";
-	Q3ValueList<State*> states = basket->usedStates();
+	QList<State*> states = basket->usedStates();
 	QString statesCss;
 	for (State::List::Iterator it = states.begin(); it != states.end(); ++it)
 		statesCss += (*it)->toCSS(imagesFolderPath, imagesFolderName, basket->Q3ScrollView::font());

commit 357264a92da533c0777cb0e8fd3bf1ce23c544b1

Fix an infinite loop introduced by 516b7c78ec52

src/note.cpp
@@ -1614,22 +1614,22 @@ void substractRectOnAreas(const QRect &rectToSubstract, QList<QRect> &areas, boo
 		if (rect.intersects(rectToSubstract)) {
 			// Create the top rectangle:
 			if (rectToSubstract.top() > rect.top()) {
-				it = areas.insert(it,QRect(rect.left(), rect.top(), rect.width(), rectToSubstract.top() - rect.top()));
+				areas.insert(it,QRect(rect.left(), rect.top(), rect.width(), rectToSubstract.top() - rect.top()));
 				rect.setTop(rectToSubstract.top());
 			}
 			// Create the bottom rectangle:
 			if (rectToSubstract.bottom() < rect.bottom()) {
-				it = areas.insert(it, QRect(rect.left(), rectToSubstract.bottom() + 1, rect.width(), rect.bottom() - rectToSubstract.bottom()));
+				areas.insert(it, QRect(rect.left(), rectToSubstract.bottom() + 1, rect.width(), rect.bottom() - rectToSubstract.bottom()));
 				rect.setBottom(rectToSubstract.bottom());
 			}
 			// Create the left rectangle:
 			if (rectToSubstract.left() > rect.left()) {
-				it = areas.insert(it, QRect(rect.left(), rect.top(), rectToSubstract.left() - rect.left(), rect.height()));
+				areas.insert(it, QRect(rect.left(), rect.top(), rectToSubstract.left() - rect.left(), rect.height()));
 				rect.setLeft(rectToSubstract.left());
 			}
 			// Create the right rectangle:
 			if (rectToSubstract.right() < rect.right()) {
-				it = areas.insert(it, QRect(rectToSubstract.right() + 1, rect.top(), rect.right() - rectToSubstract.right(), rect.height()));
+				areas.insert(it, QRect(rectToSubstract.right() + 1, rect.top(), rect.right() - rectToSubstract.right(), rect.height()));
 				rect.setRight(rectToSubstract.right());
 			}
 			// Remove the rectangle if it's entirely contained: