42ac9fa90cd57e238f29ca03dcfe4c41b9ab22e1
Dr. Robert Marmorstein <robert@narnia.homeunix.com>
2009-07-31 01:53:11 -0400

    Fix a warning about system tray not having an icon

--- src/bnpview.cpp
+++ src/bnpview.cpp
@@ -179,6 +179,7 @@ void BNPView::lateInit()
 
     /* System tray icon */
     Global::systemTray = new SystemTray(Global::mainWindow());
+    Global::systemTray->setIcon(QIcon(":/images/cr22-app-basket"));
     connect(Global::systemTray, SIGNAL(showPart()), this, SIGNAL(showPart()));
     if (Settings::useSystray())
         Global::systemTray->show();
