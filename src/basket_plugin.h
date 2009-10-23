8f686f17bdf3b0a94d903a92cdd8527260c807e7
Dr. Robert Marmorstein <robert@narnia.homeunix.com>
2009-10-22 22:57:50 -0400

    Rename image files and remove kontactinterfaces.
    We don't need kontactinterfaces any more, because the kdepim folks have
    made it a public API in kdepimlibs.  We can simply use the system code
    instead of keeping our own copy.
    
    Also, rename images so they show up in the hicolor folder.  The
    crystalsvg stuff doesn't work any more.

--- src/basket_plugin.h
+++ src/basket_plugin.h
@@ -21,15 +21,15 @@
 #ifndef BASKET_PLUGIN_H
 #define BASKET_PLUGIN_H
 
-#include <kontactinterfaces/plugin.h>
+#include <kontactinterface/plugin.h>
 #include <KDE/KParts/Part>
 
-class BasketPlugin : public Kontact::Plugin
+class BasketPlugin : public KontactInterface::Plugin
 {
     Q_OBJECT
 
 public:
-    BasketPlugin(Kontact::Core *core, const QVariantList &);
+    BasketPlugin(KontactInterface::Core *core, const QVariantList &);
     ~BasketPlugin();
 
     virtual void readProperties(const KConfigGroup &config);
